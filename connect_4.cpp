b5daeb920a4be0c54c534050d2682d631cb1c9de
dkoMines <dko@mymail.mines.edu>
2020-02-21 11:39:56 -0700

    Score counter works

--- connect_4.cpp
+++ connect_4.cpp
@@ -14,40 +14,53 @@ using namespace std;
 class Connect4{
 
 private:
+
+    const int MAX_ROWS = 6;
+    const int MAX_COLS = 7;
+
     int board[6][7];
+    int player1Score = 0;
+    int player2Score = 0;
 public:
     Connect4(){
-        for (int i=0;i<6;i++){
-            for (int j=0;j<7;j++){
+        for (int i=0;i<MAX_ROWS;i++){
+            for (int j=0;j<MAX_COLS;j++){
                 board[i][j] = 0;
             }
         }
     }
 
+    // Prints the board
     void printBoard(){
         cout << " -----------------" << endl;
-        for (int i=5;i>=0;i--){
+        for (int i=MAX_ROWS-1;i>=0;i--){
             cout << " | ";
-            for (int j=0;j<7;j++){
+            for (int j=0;j<MAX_COLS;j++){
                 cout << board[i][j] << " ";
             }
             cout << " | " << endl;
         }
         cout << " -----------------" << endl;
+        cout << endl;
+        countScore();
+        cout << "Player 1 Score: " << player1Score << endl;
+        cout << "Player 2 Score: " << player2Score << endl;
     }
 
+    // Used for generating board from input file
     void replaceRow(string row, int countRow){
         int z;
         int countCol=0;
         for (char x : row){
             z = x-48;
-            board[5-countRow][countCol] = z;
+            board[MAX_ROWS-1-countRow][countCol] = z;
             countCol ++;
         }
     }
 
+    // Will return false if all 42 pieces are filled
     bool canGo(){
-        for (int i=0;i<7;i++){
+        for (int i=0;i<MAX_COLS;i++){
             if (!filled(i)){
                 return false;
             }
@@ -57,7 +70,7 @@ public:
 
     // Returns if colNum is filled
     bool filled(int colNum){
-        if (board[5][colNum]!=0){
+        if (board[MAX_ROWS-1][colNum]!=0){
             return true;
         }
         return false;
@@ -65,13 +78,65 @@ public:
 
     bool placePiece(int colNum, int piece){
         if (filled(colNum)) return false;
-        for (int i=0;i<6;i++){
+        for (int i=0;i<MAX_ROWS;i++){
             if (board[i][colNum]==0){
                 board[i][colNum] = piece;
                 return true;
             }
         }
         return false;
+    }
+
+    // Calculates both players scores
+    void countScore(){
+        player1Score = 0;
+        player2Score = 0;
+        // Horizontal Count
+        for (int i=0;i<MAX_ROWS;i++){
+            for (int j=0;j<MAX_COLS-3;j++){
+                if (board[i][j]==1 && board[i][j+1]==1 && board[i][j+2]==1 && board[i][j+3]==1){
+                    player1Score ++;
+                }
+                if (board[i][j]==2 && board[i][j+1]==2 && board[i][j+2]==2 && board[i][j+3]==2){
+                    player2Score ++;
+                }
+            }
+        }
+        // Vertical Count
+        for (int j=0;j<MAX_COLS;j++){
+            for (int i=0;i<MAX_ROWS-3;i++){
+                if (board[i][j]==1 && board[i+1][j]==1 && board[i+2][j]==1 && board[i+3][j]==1){
+                    player1Score ++;
+                }
+                if (board[i][j]==2 && board[i+1][j]==2 && board[i+2][j]==2 && board[i+3][j]==2){
+                    player2Score ++;
+                }
+            }
+        }
+        // Diagonal Count
+        // ~ Bottom Left to Top Right
+        for (int i=0;i<MAX_ROWS-3;i++){
+            for (int j=0;j<MAX_COLS-3;j++){
+                if (board[i][j]==1 && board[i+1][j+1]==1 && board[i+2][j+2]==1 && board[i+3][j+3]==1){
+                    player1Score ++;
+                }
+                if (board[i][j]==2 && board[i+1][j+1]==2 && board[i+2][j+2]==2 && board[i+3][j+3]==2){
+                    player2Score ++;
+                }
+            }
+        }
+        // ~ Bottom Right to Top Left
+        for (int i=0;i<MAX_ROWS-3;i++){
+            for (int j=MAX_COLS-1;j>=3;j--){
+                if (board[i][j]==1 && board[i+1][j-1]==1 && board[i+2][j-2]==1 && board[i+3][j-3]==1){
+                    player1Score ++;
+                }
+                if (board[i][j]==2 && board[i+1][j-1]==2 && board[i+2][j-2]==2 && board[i+3][j-3]==2){
+                    player2Score ++;
+                }
+            }
+        }
+
     }
 
 
@@ -122,7 +187,26 @@ int main(int argc, char ** argv){
 
     
 
+    // one-move
+    if (gameMode == 1){
+        // 1) Read input file and initialize board state and score
 
+        // 2) Print current board state and score and end if full
+            myGame->printBoard();
+            if (myGame->canGo()) {
+                cout << "Board is full. Game Over" << endl;
+                goto programEnd;
+            }
+
+        // 3) Cpu makes its next move
+
+        // 4) Print current board state and score
+
+        // 5) Save current board state to the output file 
+
+        // 6) Exit
+        goto programEnd;
+    }
 
 
 
@@ -134,7 +218,7 @@ int main(int argc, char ** argv){
         if (true) goto step5;
 
 step2:
-        // Step 2) print game board. Exit if board is full
+        // Step 2) print game board and score. Exit if board is full
         myGame->printBoard();
         if (myGame->canGo()) {
             cout << "Board is full. Game Over" << endl;
@@ -147,7 +231,7 @@ step2:
         // Step 4) Save board state in computer.txt
 
 step5:
-        // Step 5) print game board. Exit if board is full
+        // Step 5) print game board and score. Exit if board is full
         myGame->printBoard();
         if (myGame->canGo()) {
             cout << "Board is full. Game Over" << endl;
