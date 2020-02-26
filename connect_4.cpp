9f8ad7e7c3ac38718bc7d234c338abc060b88d9e
dkoMines <dko@mymail.mines.edu>
2020-02-25 18:43:51 -0700

    The utility function should be complete. Minimax function is still flawed and will not work

--- connect_4.cpp
+++ connect_4.cpp
@@ -12,6 +12,15 @@
 
 using namespace std;
 
+
+const int MAX_ROWS = 6;
+const int MAX_COLS = 7;
+
+int flipPlayer(int playerTurn){
+    if (playerTurn==1) return 2;
+    return 1;
+}
+
 class Connect4{
     /**
      * Game is set up on a double array board
@@ -20,9 +29,6 @@ class Connect4{
      **/
 
 private:
-
-    const int MAX_ROWS = 6;
-    const int MAX_COLS = 7;
 
     int board[6][7];
     int player1Score = 0;
@@ -33,6 +39,25 @@ public:
         for (int i=0;i<MAX_ROWS;i++){
             for (int j=0;j<MAX_COLS;j++){
                 board[i][j] = 0;
+            }
+        }
+    }
+
+    int getPiece(int i, int j){
+        return board[i][j];
+    }
+    int getPlayer1Score(){
+        return player1Score;
+    }
+    int getPlayer2Score(){
+        return player2Score;
+    }
+
+    // Copies board from another game
+    void copyBoard(Connect4* otherGame){
+        for (int i=0;i<MAX_ROWS;i++){
+            for (int j=0;j<MAX_COLS;j++){
+                board[i][j] = otherGame->getPiece(i,j);
             }
         }
     }
@@ -165,13 +190,168 @@ public:
         outFile.close();
     }
 
+    float evaluationFunction(){ // This will give a utility 
+        float weighting[6] = {1,.5,.25}; // This is the weighting of our utilities
+        // First  is straight up 4 in a rows
+        // Second  is 3/4 in a row threats
+        // Third  is 2/4 threats
+        pair<float,float> finalScore;
+        float p1Eval = player1Score;
+        float p2Eval = player2Score;
+        finalScore.first = player1Score*weighting[0];
+        finalScore.second = player2Score*weighting[0];
 
+        // Additional ways to build scores
+        // Check threat cells ~ if can get 4 in a row. 
+        // Horizontal Utility
+        for (int pieceToCheck = 1; pieceToCheck < 3; pieceToCheck++){
+            float utilityAdd = 0;
+            for (int i=0;i<MAX_ROWS;i++){
+                float countPoss = 0;
+                float countPieces = 0;
+                for (int j=0;j<MAX_COLS;j++){
+                    if (board[i][j]==pieceToCheck){
+                        countPieces ++;
+                    } else if (board[i][j] == 0){
+                        countPoss ++;
+                    } else {
+                        // Enemy Piece is blocking. Reset counters
+                        if (countPoss >=4 && countPieces==2){ // There is a threat filled by 2/4 pieces
+                            utilityAdd += weighting[2];
+                        } else if (countPoss >=4 && countPieces==3){ // There is a threat filled by 3/4 pieces
+                            utilityAdd += weighting[1];
+                        } else if (countPoss >=4 && countPieces>=4 && countPoss>countPieces){  // This should be a similar threat as 3 threat as it could be 4 that goes to 5 ~ +1 points
+                            utilityAdd += weighting[1];
+                        }
+                        countPoss = 0;
+                        countPieces = 0;
+                    }
+                }
+            }
+            if (pieceToCheck==1){
+                finalScore.first += utilityAdd;
+            } else {
+                finalScore.second += utilityAdd;
+            }
+        }
+        // Vertical Utility
+        for (int pieceToCheck = 1; pieceToCheck < 3; pieceToCheck++){
+            float utilityAdd = 0;
+            for (int j=0;j<MAX_COLS;j++){
+                int pieceCount = 0;
+                for (int i=0;i<MAX_ROWS;i++){
+                    if (board[i][j]==pieceToCheck){
+                        pieceCount ++;
+                    } else if (board[i][j] != 0){
+                        pieceCount = 0;
+                    }
+                }
+                if (pieceCount==2){
+                    utilityAdd += weighting[2]; // 2 in a row threat
+                } else if (pieceCount==3){
+                    utilityAdd += weighting[1]; // 3 in a row threat
+                }
+            }
+            if (pieceToCheck==1){
+                finalScore.first += utilityAdd;
+            } else {
+                finalScore.second += utilityAdd;
+            }
+        }
+        // Diagonal Utility
+        // ~ Bottom Left to Top Right
+        for (int pieceToCheck = 1; pieceToCheck < 3; pieceToCheck++){
+            float utilityAdd = 0;
+            for (int i=0;i<MAX_ROWS-3;i++){
+                for (int j=0;j<MAX_COLS-3;j++){
+                    int pieces = 0;
+                    for (int count = 0;count<4;count++){
+                        if (board[i+count][j+count]==pieceToCheck){
+                            pieces++;
+                        } else if (board[i+count][j+count]!=pieceToCheck){
+                            pieces = -10;
+                        }
+                    }
+                    if (pieces==2){
+                        utilityAdd += weighting[2]; // 2/4 threats
+                    } else if (pieces==3){
+                        utilityAdd += weighting[1]; // 3/4 threats
+                    }
+                }
+            }
+            // ~ Bottom Right to Top Left
+            for (int i=0;i<MAX_ROWS-3;i++){
+                for (int j=MAX_COLS-1;j>=3;j--){
+                    int pieces = 0;
+                    for (int count = 0;count<4;count++){
+                        if (board[i+count][j-count]==pieceToCheck){
+                            pieces++;
+                        } else if (board[i+count][j-count]!=pieceToCheck){
+                            pieces = -10;
+                        }
+                    }
+                    if (pieces==2){
+                        utilityAdd += weighting[2]; // 2/4 threats
+                    } else if (pieces==3){
+                        utilityAdd += weighting[1]; // 3/4 threats
+                    }
+                }
+            }
+            if (pieceToCheck==1){
+                finalScore.first += utilityAdd;
+            } else {
+                finalScore.second += utilityAdd;
+            }
+        }
+
+
+        return finalScore.first - finalScore.second;
+    }
 };
 
-int flipPlayer(int playerTurn){
-    if (playerTurn==1) return 2;
+// This will help us determine where the player should go to next. Pair is returned as col, utility
+pair<int,int> minimax(int depthLeft, int playerTurn, Connect4* myGame, bool max){
+    vector<pair<int,int>> utilityTracker; // For the pair, col num, then it is the utility.
+    depthLeft --;
+    int utility;
+    // Check the cols
+    for (int j=0;j<MAX_COLS;j++){
+        Connect4* newGame = new Connect4();
+        newGame->copyBoard(myGame);
+        if (!newGame->filled(j)){
+            // It is possible to go here
+            newGame->placePiece(j,playerTurn);
+            if (depthLeft>0){
+                utility = minimax(depthLeft, flipPlayer(playerTurn) ,newGame, !max).second;
+            } else if (depthLeft==0){
+                // Gotta get those Utility numbers
+                // Player1 up is positive, Player2 up is negative
+                utility = newGame->evaluationFunction();
+            }
+            pair<int,int> colUtil;
+            colUtil.first = j;
+            colUtil.second = utility;
+            utilityTracker.push_back(colUtil);
+        }
+    }
+    // MMk, we have utility for all 7 rows. 
+    return utilityTracker[0];
+
+
+
+
+
+}
+
+int cpuPlays(int depth, int playerTurn, Connect4* myGame){
+    minimax(depth,playerTurn,myGame, true);
+
+    
     return 1;
 }
+
+
+
 
 
 int main(int argc, char ** argv){
